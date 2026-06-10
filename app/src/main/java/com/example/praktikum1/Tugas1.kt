83f782591365ec88d65fe67e77b4ecc6ace8e676
dheazahra27 <dheaz6691@gmail.com>
2026-06-10 21:44:07 +0800

    Commit

--- app/src/main/java/com/example/praktikum1/Tugas1.kt
+++ app/src/main/java/com/example/praktikum1/Tugas1.kt
@@ -1,6 +1,9 @@
 package com.example.praktikum1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,20 @@ class Tugas1 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val etNim = findViewById<EditText>(R.id.et_NIM)
+        val etNama = findViewById<EditText>(R.id.et_namaLengkap)
+        val etProdi = findViewById<EditText>(R.id.et_prodi)
+        val btnProses = findViewById<Button>(R.id.bt_Proses)
+
+        btnProses.setOnClickListener {
+            val nim = etNim.text.toString()
+            val nama = etNama.text.toString()
+            val prodi = etProdi.text.toString()
+
+            Toast.makeText(this, "Halo, perkenalkan saya $nama dengan NIM $nim pada Program Studi $prodi",
+                Toast.LENGTH_SHORT).show()
         }
     }
 }
